8fd28eaffe42880497495007a4debce3c8ceae76
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 11:41:27 -0500

    updated exercise model with all fields given

--- models/exercise.js
+++ models/exercise.js
@@ -6,19 +6,47 @@ const Schema = mongoose.Schema;
 // If the exercise is a cardio exercise, I should be able to track my distance traveled.
 
 const exerciseSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: ""
-  },
-  value: {
-    type: Number,
-    required: ""
-  },
   date: {
     type: Date,
     default: Date.now
-  }
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: ""
+      },
+      type: {
+        type: String,
+        trim: true
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      duration: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0
+  },
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
