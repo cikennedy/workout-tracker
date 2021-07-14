2ae5396da406cc327330865915f864bac9a02681
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 11:28:47 -0500

    changed models directory files, added comments from README instructions for what needs to be added to exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// I should also be able to track the name, type, weight, sets, reps, and duration of exercise. 
+// If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
 const exerciseSchema = new Schema({
   name: {
     type: String,
