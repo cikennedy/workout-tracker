67e5a48e7617d180a21417b85f68603ef6b808c7
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 12:16:46 -0500

    added all necessary html routes per the public directory

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const path = require("path");
+
+// '/' should return the `index.html` file.
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// `/exercise` should return the `exercise.html` file.
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+// `/stats` should return the `exercise.html` file.
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
 
 module.exports = router;
