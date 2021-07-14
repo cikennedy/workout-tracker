060cf5775e3d7cce1f7d180a949167fc954ef3e9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 13:06:02 -0500

    updating apiRoutes, requires testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
-const db = require("../models");
+// const db = require("../models");
+const Excercise = require("../models/exercise");
+
+// view all workouts 
+router.get('/workouts', (req, res) => {
+    Excercise.find()
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/workouts', (req, res) => {
+    Excercise.create({})
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// 
 
 module.exports = router;
