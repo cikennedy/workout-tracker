25df75e43943519f362d89a56cfefd9d44cf0372
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 11:59:19 -0500

    changed server.js to include the api and html routes, other necessary changes

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-// Add routes, both API and view
-app.use(routes);
+// Routes 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker");
